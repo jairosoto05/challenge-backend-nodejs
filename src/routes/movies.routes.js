87ef4ef5d90fee40bd1eea9f267a6273ce18c880
Jairo Soto <jairosoto05@gmail.com>
2022-05-03 02:12:34 -0400

    movies.routes.js corrected

--- src/routes/movies.routes.js
+++ src/routes/movies.routes.js
@@ -1,17 +1,16 @@
 const Router = require('express');
 const router = Router();
 
-const { uploadImage } = require('../middleware/uploadFile');
 const { createMovie, getMovie, updateMovie, deleteMovie, searchMovie } = require('../controllers/movie.controller');
 
 router.route('/movies')
     .get(searchMovie)
-    .post(uploadImage, createMovie)
+    .post(createMovie)
 
 router.route('/movies/:id')
     .get(getMovie)
     .delete(deleteMovie)
-    .put(uploadImage, updateMovie)
+    .put(updateMovie)
 
 
 module.exports = router;
